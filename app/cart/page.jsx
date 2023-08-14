d0c556c3b899b34d5b6e22639a38641b77f243a2
e-afzal <essam.afzal@outlook.com>
2023-08-14 15:34:32 +0400

    Cart page: Condition rendering based on # of cart items

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -62,67 +62,81 @@ const Cart = () => {
         <NavbarMobile />
         <main id={styles.main}>
           <h2 className={styles.cart_title}>your bag</h2>
-          <div className={styles.cart_container}>
-            {/* SECTION: CART TABLE */}
-            <section id={styles.cart_table}>
-              <p className={styles.itemNumber}>
-                {totalQuantity} item{totalQuantity > 1 ? "s" : ""} in total
-              </p>
-              {/* ITEM CARD */}
-              {loadedItems?.map((item, index) => (
-                <div key={index} className={styles.item_card}>
-                  <div className={styles.item_image}>
-                    <Image
-                      src={item.productImage}
-                      width={130}
-                      height={130}
-                      alt={`${item.productTitle} image thumbnail`}
-                    />
-                  </div>
-                  <div className={styles.item_content}>
-                    <p className={styles.item_title}>{item.productTitle}</p>
-                    <p className={styles.item_perUnit}>
-                      Price per unit: ${item.productPricePerUnit}
+          {/* IF ITEMS IN CART */}
+          {loadedItems.length >= 1 && (
+            <div className={styles.cart_container}>
+              {/* SECTION: CART TABLE */}
+              <section id={styles.cart_table}>
+                <p className={styles.itemNumber}>
+                  {totalQuantity} item{totalQuantity > 1 ? "s" : ""} in total
+                </p>
+                {/* ITEM CARD */}
+                {loadedItems?.map((item, index) => (
+                  <div key={index} className={styles.item_card}>
+                    <div className={styles.item_image}>
+                      <Image
+                        src={item.productImage}
+                        width={130}
+                        height={130}
+                        alt={`${item.productTitle} image thumbnail`}
+                      />
+                    </div>
+                    <div className={styles.item_content}>
+                      <p className={styles.item_title}>{item.productTitle}</p>
+                      <p className={styles.item_perUnit}>
+                        Price per unit: ${item.productPricePerUnit}
+                      </p>
+                      <CartQuantityCounter
+                        productId={item.productId}
+                        quantity={item.quantity}
+                        incrementQuantity={incrementQuantity}
+                        decrementQuantity={decrementQuantity}
+                      />
+                      <span
+                        className={styles.removeBtn}
+                        onClick={() => removeItem(item.productId)}
+                      >
+                        remove
+                      </span>
+                    </div>
+                    <p className={styles.item_subtotal}>
+                      ${(item.quantity * item.productPricePerUnit).toFixed(2)}
                     </p>
-                    <CartQuantityCounter
-                      productId={item.productId}
-                      quantity={item.quantity}
-                      incrementQuantity={incrementQuantity}
-                      decrementQuantity={decrementQuantity}
-                    />
-                    <span
-                      className={styles.removeBtn}
-                      onClick={() => removeItem(item.productId)}
-                    >
-                      remove
-                    </span>
                   </div>
-                  <p className={styles.item_subtotal}>
-                    ${(item.quantity * item.productPricePerUnit).toFixed(2)}
+                ))}
+              </section>
+
+              {/* SECTION: CART SUMMARY */}
+              <section id={styles.cart_summary}>
+                <h4 className={styles.summary_title}>Order summary</h4>
+                <div className={styles.summary_flex}>
+                  <p className={styles.units}>Products ({totalQuantity})</p>
+                  <p className={styles.subtotal_price}>
+                    ${subTotal.toFixed(2)}
                   </p>
                 </div>
-              ))}
-            </section>
-
-            {/* SECTION: CART SUMMARY */}
-            <section id={styles.cart_summary}>
-              <h4 className={styles.summary_title}>Order summary</h4>
-              <div className={styles.summary_flex}>
-                <p className={styles.units}>Products ({totalQuantity})</p>
-                <p className={styles.subtotal_price}>${subTotal.toFixed(2)}</p>
-              </div>
-              <div className={styles.subtotal_flex}>
-                <p className={styles.subtotal_description}>
-                  Subtotal incl. Taxes
-                </p>
-                <p className={styles.subtotal_amount}>
-                  <sup>$</sup>
-                  {subTotal.toFixed(2)}
-                </p>
-              </div>
-              <Link href={"/checkout"}>go to checkout</Link>
-            </section>
-          </div>
+                <div className={styles.subtotal_flex}>
+                  <p className={styles.subtotal_description}>
+                    Subtotal incl. Taxes
+                  </p>
+                  <p className={styles.subtotal_amount}>
+                    <sup>$</sup>
+                    {subTotal.toFixed(2)}
+                  </p>
+                </div>
+                <Link href={"/checkout"}>go to checkout</Link>
+              </section>
+            </div>
+          )}
+          {/* IF NO ITEMS IN CART */}
+          {loadedItems.length === 0 && (
+            <div className={styles.cart_empty}>
+              <p>No items were found in the cart.</p>
+              <Link href={"/"} className="cta">
+                Shop now
+              </Link>
+            </div>
+          )}
         </main>
         <Footer />
       </>
